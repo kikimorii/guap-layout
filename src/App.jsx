d6a90723dcf1372c8101faf4c39331d046012b45
Oleg Fateev <fateev010405@gmail.com>
2025-10-03 12:03:47 +0300

    Hotfix: For prod

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-export const App = () => {
-  return (
-    <>
-      
-    </>
-  )
-}
-
-export default App
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import './assets/js/desktopMenu/index.js';
+import './assets/js/mobileMenu/index.js';
+import './assets/scss/main.scss';
+import { updateYear } from './assets/js/utils';
+
+updateYear(document.querySelector("#footerCopyrightYear"));
